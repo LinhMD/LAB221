2905531c63487ba4fa39097bfd1470cdfd2c6ddd
LinhMD <maidinhlinh967@gmail.com>
2020-09-17 13:02:35 +0700

    nah

--- P0001/src/utills/MyConnection.java
+++ P0001/src/utills/MyConnection.java
@@ -1,18 +1,22 @@
 package utills;
 
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class MyConnection {
     public static Connection makeConnection(){
         try {
+            Properties properties = new Properties();
+            properties.load(MyConnection.class.getResourceAsStream("config.properties"));
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=ItemManagement";
-            return DriverManager.getConnection(url, "sa", "123456");
-        } catch (ClassNotFoundException | SQLException e) {
+            String url = properties.getProperty("jdbc.url");
+            return DriverManager.getConnection(url, properties.getProperty("jdbc.account"), properties.getProperty("jdbc.password"));
+        } catch (ClassNotFoundException | SQLException | IOException e) {
             e.printStackTrace();
             return null;
         }
